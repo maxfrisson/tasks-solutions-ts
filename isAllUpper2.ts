c42717268a903503467d3868d46423366c0fb8bd
Max Frisson <max.chebonenko@gmail.com>
2021-10-28 00:19:42 +0300

    fix isAllUpper2

--- isAllUpper2.ts
+++ isAllUpper2.ts
@@ -2,7 +2,7 @@
 // If the string is empty or doesn't have any letter in it - function should return False.
 
 function isAllUpper2(text: string): boolean {
-  return text.length != 0 ? !/[a-z]/g.test(text) : false;
+  return text.length == 0 || /^[\s || 0-9]/.test(text) ? false : !/[a-z]/g.test(text);
 }
 
-console.log(isAllUpper2(""));
+console.log(isAllUpper2("ASD"));

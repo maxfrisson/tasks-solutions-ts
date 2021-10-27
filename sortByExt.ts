b91ed639b3c3f2864fea183cf826f484d6bf3696
Max Frisson <max.chebonenko@gmail.com>
2021-10-27 23:43:25 +0300

    sortByExt solution

--- sortByExt.ts
+++ sortByExt.ts
@@ -1,5 +1,13 @@
 function sortByExt(files:string[]): string[] {
-  return
+  let arr = [];
+  for (let x of files) { 
+      let a =  x.split(/[\.]/g).filter(i => i.length > 0); 
+      let b = '';
+      if (a.length >= 2 && !x.endsWith('.'))
+          b = a[a.length - 1];
+  arr.push([b, x.replace(b, '')]);
+  }
+  return arr.sort().map(x => x[1].concat(x[0]));
 }
 
 console.log(sortByExt(['1.cad', '1.bat', '1.aa', '.bat']));

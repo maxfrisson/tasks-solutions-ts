5de8472e33136bb8f5444e31b33c65322ab880f7
Max Frisson <max.chebonenko@gmail.com>
2021-10-29 02:45:30 +0300

    add findCapitals

---
+++ findCapitals.ts
@@ -0,0 +1,27 @@
+function findCapitals(word: string): number[] {
+  console.log(
+    word.match(/[A-Z]/g).map((x) => {
+      return word.indexOf(x);
+    })
+  );
+
+  let capArr = word.match(/[A-Z]/g);
+  let resArr = [];
+  for (let i = 0; i < capArr.length; i++) {
+    resArr.push(word.indexOf(word.match(/[A-Z]/g)[i]));
+  }
+
+  return resArr;
+}
+
+console.log(findCapitals("CodEWaRs"));
+
+////////////////////// CLEAR SOLUTION /////////////////////////////
+function findCapitals2(word: string): number[] {
+  return word.match(/[A-Z]/g).map((x) => {
+    return word.indexOf(x);
+  });
+}
+
+console.log("CodEWaRs");
+///////////////////////////////////////////////////////////////////

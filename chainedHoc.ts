71047c568350c0d971387ce474536a03a39555a7
Max Frisson <max.chebonenko@gmail.com>
2021-11-17 22:59:10 +0300

    fix chainedHoc

--- chainedHoc.ts
+++ chainedHoc.ts
@@ -5,7 +5,7 @@ function f3(x){ return Math.pow(x,2) }
 function f4(x){ return x.split("").concat().reverse().join("").split(" ")}
 function f5(xs){ return xs.concat().reverse() }
 function f6(xs){ return xs.join("_") }
-
+///////////////////////////////////////////////////////////////////////////////////
 function chainedHoc(functions) {
   return function inner(a) {
     for (let b = 0; b < functions.length; b++) {
@@ -16,3 +16,13 @@ function chainedHoc(functions) {
 }
 
 console.log(chainedHoc([f1, f2, f3])(2));
+///////////////////////////// SOLUTION WITH REDUCE ////////////////////////////////
+function chainedHoc2(functions) {
+  return function (input) {
+    return functions.reduce(function (input, fn) {
+      return fn(input);
+    }, input);
+  };
+}
+
+console.log(chainedHoc2([f1, f2, f3])(2));

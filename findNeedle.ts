8f9382c594fcf64f29b197946cea2188405cf4a1
Max Frisson <max.chebonenko@gmail.com>
2022-02-11 20:13:20 +0300

    add findNeedle

---
+++ findNeedle.ts
@@ -0,0 +1,7 @@
+function findNeedle(haystack: any[]): string {
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
+
+console.log(
+  findNeedle(["3", "123124234", undefined, "needle", "world", "hay", 2, "3", true, false])
+);

61969f01f21b8e5acc244b24faa4b61fadfddb85
Max Frisson <max.chebonenko@gmail.com>
2022-02-14 23:00:17 +0300

    add abbrevName

---
+++ abbrevName.ts
@@ -0,0 +1,14 @@
+function abbrevName(name: string): string {
+  return name
+    .split(" ")
+    .map((n) => n[0].toUpperCase())
+    .join(".");
+}
+
+console.log(abbrevName("Patrick Feenan"));
+
+function abbrevName2(name: string): string {
+  return name.match(/\b\w/g).join(".").toUpperCase();
+}
+
+console.log(abbrevName2("Alex Fisher"));

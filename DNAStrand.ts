b8714518268535ea4074d165c7806b1e2210999d
Max Frisson <max.chebonenko@gmail.com>
2022-02-18 22:12:48 +0300

    update DNAStrand

--- DNAStrand.ts
+++ DNAStrand.ts
@@ -6,3 +6,12 @@ function DNAStrand(dna: string): string {
 }
 
 console.log(DNAStrand("ACGA"));
+
+/////////////////////// REPLACE SOLUTION ///////////////////////////////////
+
+function DNAStrand2(dna: string): string {
+  let pairs = { A: "T", T: "A", C: "G", G: "C" };
+  return dna.replace(/./g, (x) => pairs[x]);
+}
+
+console.log(DNAStrand2("ATCG"));

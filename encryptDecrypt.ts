73ad02272d807d3c1cc3dd76f895dee306dfebd6
Max Frisson <max.chebonenko@gmail.com>
2022-05-30 02:18:49 +0300

    update encryptDecrypt

--- encryptDecrypt.ts
+++ encryptDecrypt.ts
@@ -1,8 +1,32 @@
-function encrypt(text: any, n: number) {
-  if (text === null) return text
-  let newText = text.split("").filter((_:string,i:number) => i%2).join('') + text.split("").filter((_:string,i:number) => !(i%2)).join('')
-  if (n > 0) return encrypt(newText,n-1);
+function encrypt(text: string, n: number) {
+  if (text === null) return text;
+  let newText =
+    text
+      .split("")
+      .filter((_: string, i: number) => i % 2)
+      .join("") +
+    text
+      .split("")
+      .filter((_: string, i: number) => !(i % 2))
+      .join("");
+  if (n > 0) return encrypt(newText, n - 1);
   return text;
 }
 
-console.log(encrypt("012345", 1));
+console.log(encrypt("This kata is very interesting!", 1));
+
+function decrypt(encryptedText: string, n: number) {
+  if (encryptedText === null) return encryptedText;
+  let center = Math.floor(encryptedText.length / 2);
+  let even = encryptedText.slice(center).split("");
+  let odd = encryptedText.slice(0, center).split("");
+  let arr = [];
+  while (odd.length || even.length) {
+    arr.push(even.shift(), odd.shift());
+  }
+  let newText = arr.join("");
+  if (n > 0) return decrypt(newText, n - 1);
+  return encryptedText;
+}
+
+console.log(decrypt(" Tah itse sits!", 3));

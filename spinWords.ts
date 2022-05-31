04e0490f67e2511af4df9a58e2ff4ba0ce432abe
Max Frisson <max.chebonenko@gmail.com>
2022-06-01 01:04:48 +0300

    update spinWords

--- spinWords.ts
+++ spinWords.ts
@@ -1,8 +1,22 @@
-function spinWords (str: string): string {
-  return str.split(" ").map(x => x.length > 4 ? x.split("").reverse().join("") : x).join(" ")
+function spinWords(str: string): string {
+  return str
+    .split(" ")
+    .map((x) => (x.length > 4 ? x.split("").reverse().join("") : x))
+    .join(" ");
 }
 
-console.log(spinWords("Welcome"))
-console.log(spinWords("This is a test"))
-console.log(spinWords("Hey fellow warriors"))
-console.log(spinWords("Just kidding there is still one more"))
+console.log(spinWords("Welcome"));
+console.log(spinWords("This is a test"));
+console.log(spinWords("Hey fellow warriors"));
+console.log(spinWords("Just kidding there is still one more"));
+
+/////////////////////////////// REGEX REPLACE SOLUTION /////////////////////////////////////
+
+function spinWords2(string: string): string {
+  return string.replace(/\w{5,}/, (w) => w.split("").reverse().join(""));
+}
+
+console.log(spinWords2("Welcome"));
+console.log(spinWords2("This is a test"));
+console.log(spinWords2("Hey fellow warriors"));
+console.log(spinWords2("Just kidding there is still one more"));

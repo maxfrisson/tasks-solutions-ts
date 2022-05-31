99ec0be7cc7cd59ba453d49303453d10ced0ba59
Max Frisson <max.chebonenko@gmail.com>
2022-06-01 00:52:25 +0300

    update generateHashtag

--- generateHashtag.ts
+++ generateHashtag.ts
@@ -10,3 +10,16 @@ function generateHashtag(str: string): string | boolean {
 console.log(generateHashtag("Codewars is nice"));
 console.log(generateHashtag("    hello     world   "));
 console.log(generateHashtag(" ".repeat(200)));
+
+/////////////////////////// REDUCE SOLUTION ///////////////////////////
+function generateHashtag2(str: string): string | boolean {
+  let hastag = str.split(" ").reduce(function (tag, word) {
+    return tag + word.charAt(0).toUpperCase() + word.substring(1);
+  }, "#");
+
+  return hastag.length == 1 || hastag.length > 140 ? false : hastag;
+}
+
+console.log(generateHashtag2("Codewars is nice"));
+console.log(generateHashtag2("    hello     world   "));
+console.log(generateHashtag2(" ".repeat(200)));

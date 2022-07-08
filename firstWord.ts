f95813670c7ab2116223b5c2331f8685b59ef181
Max Frisson <max.chebonenko@gmail.com>
2022-07-08 17:34:36 +0300

    fix firstWord

--- firstWord.ts
+++ firstWord.ts
@@ -1,7 +1,7 @@
 // You are given a string where you have to find its first word.
 
 function firstWord(text: string): string {
-  return /[\w']+/.exec(text)[0];
+  return /[\w]+/.exec(text)![0];
 }
 
 console.log(firstWord("Hello world"));
